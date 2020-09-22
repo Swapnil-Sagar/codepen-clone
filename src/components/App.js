24f11c59e87eb68e2ca74b637e144ad782885184
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-09-23 04:12:23 +0530

    Added footer component and changed Readme.md

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 import useLocalStorage from "../hooks/useLocalStorage";
+import Footer from "./Footer";
 
 function App() {
   const [html, setHtml] = useLocalStorage("html", "");
@@ -49,11 +50,12 @@ function App() {
           srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
-          frameborder="0"
+          frameBorder="0"
           width="100%"
           height="100%"
         />
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="row">
+        <h4 className="footer-title">Code Draft - Live Site Maker</h4>
+        <a
+          href="https://github.com/Swapnil-Sagar/codepen-clone"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github"
+        >
+          <FaGithub />
+        </a>
+        <p className="footer-by">Made with 💙 by Swapnil Sagar</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
